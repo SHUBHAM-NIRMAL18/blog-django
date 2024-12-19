6a2a211f710a47dd807278ff691dd00e0cdae6ff
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-19 15:55:27 +0545

    Use CreateView for adding Blog

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
-# Using ListView for Listing Posts
+# Using ListView for Listing Blog
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
 
-# Using DetailView for display the Single detailed Post
+# Using DetailView for display the Single detailed Blog
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    
+
+
+#Using CreateView for adding form to add Blog
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
