df9871d98b61679fc2951b910d83c989edab17af
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-19 16:04:14 +0545

    URL Config for adding new blog

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),# URL Config for Detail Blog
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), #Url Config for blog form
 ]
