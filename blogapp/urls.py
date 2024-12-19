b95af3e6ada4164934edd5cb24641c832a48b291
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-19 18:35:07 +0545

    URL Config for deleting blog

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),# URL Config for Detail Blog
     path('post/new/', BlogCreateView.as_view(), name='post_new'), #Url Config for blog form
-    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit') #URL Config for Editing Blog
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'), #URL Config for Editing Blog
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'), #URL Config for deleting Blog
+
 
 ]
