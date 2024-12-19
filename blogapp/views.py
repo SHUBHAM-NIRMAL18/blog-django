82d49fb3a2206c09adfca898b7d86a9afb62509b
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-19 18:32:34 +0545

    Use DeleteView for deleting Blog

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy 
 from .models import Post
 
 # Using ListView for Listing Blog
@@ -26,3 +27,10 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+#Using DeleteView for deleteing the Blog
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
