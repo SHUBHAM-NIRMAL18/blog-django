8a238f485d0af3e3d452b4d971fd74979cb90412
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-22 16:55:13 +0545

    chore: update setting.py to include 'session' app

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blogapp", #defining the blogapp
+    "session", #definig the session
 ]
 
 MIDDLEWARE = [
