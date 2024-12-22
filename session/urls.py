aad0c5f97b18428df163c4ff82414462c4657037
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-22 17:16:07 +0545

    refactor: update the urls.py of session

--- session/urls.py
+++ session/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, DashboardView
 
 urlpatterns = [
-    path('', LoginView.as_view(), name='login'), #this url config follow session/login
+    path('', LoginView.as_view(), name='login'), #this url config follow session/
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),#this url config follow session/dashbaord
 ]
