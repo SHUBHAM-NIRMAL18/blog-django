fb33a1fb2ca3411f59bd79aab353966b74cea867
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-22 16:35:04 +0545

    feat: add LoginView with static username and password

--- session/views.py
+++ session/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+# Simple Staic login for session demo
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username == 'shubham' and password == 'pass123':
+            request.session['username'] = username # Save shubham to session
+            return redirect('dashboard')
+        return render(request, 'login.html', {'error':'Incorrect password'})
+
+
