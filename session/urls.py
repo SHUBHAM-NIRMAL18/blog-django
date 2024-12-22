df9bc86ebbff4d9a9c88fbaec9b7ec55f63f403c
Shubham Nirmal <suvamniraml456@gmail.com>
2024-12-22 17:29:10 +0545

    refactor: update the urls.py of session for logout

--- session/urls.py
+++ session/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginView, DashboardView
+from .views import LoginView, DashboardView, LogoutView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'), #this url config follow session/
     path('dashboard/', DashboardView.as_view(), name='dashboard'),#this url config follow session/dashbaord
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
